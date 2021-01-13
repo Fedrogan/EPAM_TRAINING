e22f8fd564870b5506422669932bf50fa68bed40
Fedrogan <75548970+Fedrogan@users.noreply.github.com>
2021-01-14 01:11:12 +0300

    task 2 Solution

---
+++ TASKS1/src/com/epam/Cycles/task2/Solution.java
@@ -0,0 +1,24 @@
+package com.epam.Cycles.task2;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int a = -5;
+        int b = 10;
+        double h = 0.5;
+
+        for (double i = a; i <= b; ) {
+            System.out.println(func(i));
+            i += h;
+        }
+
+    }
+
+    public static double func(double x) {
+        if (x > 2) {
+            return x;
+        } else {
+            return -x;
+        }
+    }
+}
